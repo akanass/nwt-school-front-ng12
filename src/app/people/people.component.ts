dfb37e35e116ec6028ff888ebabc4a214fef637c
akanass <akanass@icloud.com>
2021-10-01 10:56:03 +0200

    step-12

--- src/app/people/people.component.ts
+++ src/app/people/people.component.ts
@@ -3,6 +3,8 @@ import { Person } from '../shared/types/person.type';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment';
 import { defaultIfEmpty, filter } from 'rxjs';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { DialogComponent } from '../shared/dialog/dialog.component';
 
 @Component({
   selector: 'nwt-people',
@@ -14,13 +16,18 @@ export class PeopleComponent implements OnInit {
   private _people: Person[];
   // private property to store all backend URLs
   private readonly _backendURL: any;
+  // private property to store dialogStatus value
+  private _dialogStatus: string;
+  // private property to store dialog reference
+  private _peopleDialog: MatDialogRef<DialogComponent> | undefined;
 
   /**
    * Component constructor
    */
-  constructor(private _http: HttpClient) {
+  constructor(private _http: HttpClient, private _dialog: MatDialog) {
     this._people = [];
     this._backendURL = {};
+    this._dialogStatus = 'inactive';
 
     // build backend base url
     let baseUrl = `${environment.backend.protocol}://${environment.backend.host}`;
@@ -41,6 +48,13 @@ export class PeopleComponent implements OnInit {
   }
 
   /**
+   * Returns private property _dialogStatus
+   */
+  get dialogStatus(): string {
+    return this._dialogStatus;
+  }
+
+  /**
    * OnInit implementation
    */
   ngOnInit(): void {
@@ -58,5 +72,21 @@ export class PeopleComponent implements OnInit {
   delete(person: Person): void {
     this._http.delete(this._backendURL.onePeople.replace(':id', person.id))
       .subscribe({ next: () => this._people = this._people.filter((p: Person) => p.id !== person.id) });
+  }
+
+  /**
+   * Function to display modal
+   */
+  showDialog(): void {
+    // set dialog status
+    this._dialogStatus = 'active';
+
+    // open modal
+    this._peopleDialog = this._dialog.open(DialogComponent, {
+      width: '500px'
+    });
+
+    // subscribe to afterClosed observable to set dialog status and do process
+    this._peopleDialog.afterClosed().subscribe(_ => this._dialogStatus = 'inactive');
   }
 }
